ab6b7828e1ad8125e3dfbeac5c348e6b3509dcba
mikebannis <mikebannis@gmail.com>
2016-10-16 20:21:53 -0600

    generally works, needs plan functionality to be implemented

--- test.py
+++ test.py
@@ -5,6 +5,13 @@ import sys
 # Codes for RAS output types
 WSEL = 2
 MIN_CH_EL = 5
+STA_WS_LFT = 36
+STA_WS_RGT = 37
+
+# Values for below codes should probably be pulled from geometry
+RIGHT_STA = 264  # right station of a XS
+LEFT_STA = 263  # left station of a XS
+
 
 class Profile(object):
     def __init__(self, name, code, rc):
@@ -13,7 +20,7 @@ class Profile(object):
         self.rc = rc   # RasController object
 
     def __repr__(self):
-        return self.name + '/' +str(self.code)
+        return 'Profile name = "'+self.name + '", Profile code = "' + str(self.code)+'"'
 
 
 class River(object):
@@ -23,6 +30,7 @@ class River(object):
         self.rc = rc   # RasController object
         self.reaches = self._get_reaches()  # list of Reach objects
 
+    # TODO -  the reach code should probably be pulled from the rascontller, although i+1 seems to work
     def _get_reaches(self):
         """
         Gets list of reaches for river represented by self
@@ -47,6 +55,7 @@ class Reach(object):
         self.rc = self.river.rc   # RasController object
         self.nodes = self._get_nodes()  # list of Reach objects
 
+    # TODO -  the reach code should probably be pullled from the rascontller, although i+1 seems to work
     def _get_nodes(self):
         """
         Gets list of reaches for river represented by self
@@ -71,32 +80,45 @@ class Node(object):
         self.node_id = node_id  # Node name, string
         self.node_type = node_type  # node type: '' (XS), 'BR', 'Culv', 'IS', ... etc
         self.code = code  # Node code, int - these start at 1, not 0
+        # The line below is how the code should really be gotten
+        # self.code = self.rc.com_rc.Geometry_GetNode(self.river.code, self.reach.code, self.node_id)[0]
         self.reach = reach
         self.river = self.reach.river
         self.rc = self.river.rc   # RasController object
-        self.special_code = self.rc.com_rc.Geometry_GetNode(self.river.code, self.reach.code, self.node_id)[0]
 
-    def value(self, value_type):
+    def value(self, profile, value_type):
         # TODO - this should likely check if this is a bridge due to the 0 in output_nodeoutput
-        return self.rc.output_nodeoutput(self.river.code, self.reach.code, self.code, value_type)
+        return self.rc.output_nodeoutput(self.river.code, self.reach.code, self.code, profile.code, value_type)
 
     def __repr__(self):
-        return self.node_id + '/' + self.node_type + '/' + str(self.code)
+        if self.node_type == '':
+            node_type = 'XS'
+        else:
+            node_type = self.node_type
+        return 'Node name ="' + self.node_id + '", Node type ="' + node_type + '", Node code = "' + str(self.code) + \
+                '"'
 
 
 class RasController(object):
     def __init__(self):
         # See if RAS is open and abort if so
-        if not True:
+        if True:
             for p in psutil.process_iter():
                 try:
                     if p.name() == 'ras.exe':
                         sys.exit('HEC-RAS appears to be open. Please close HEC-RAS. Exiting.')
                 except psutil.Error:
                     pass
+
         # RAS is not open yet, open it
         self.com_rc = win32com.client.DispatchEx('RAS41.HECRASController')
         # self.com_rc = win32com.client.DispatchEx('RAS500.HECRASController')
+
+    def get_plans(self):
+        """
+        Returns list of Plan objects
+        """
+        pass
 
     def get_profiles(self):
         """
@@ -122,13 +144,12 @@ class RasController(object):
             rivers.append(new_prof)
         return rivers
 
-    # TODO - remove once obsolete
-    def _current_plan_file(self):
+    def is_output_current(self):
         """
-        Returns path and file name of current plan file
-        :return: string
+        Returns True if output matches current plan
         """
-        return self.com_rc.CurrentPlanFile()
+        # PlanOutput_IsCurrent() - I may be misunderstanding how this works
+        pass
 
     def open_project(self, project):
         """
@@ -137,15 +158,25 @@ class RasController(object):
         """
         self.com_rc.Project_Open(project)
 
-    def _plan_names(self, basedir=True):
+    def run_current_plan(self):
         """
-        returns list of plan names
-        :param basedir: ???? unknown
-        :return: list of strings
+        Run current plan in RAS
+        :return: status, messages - ??, ??
         """
-        _, names, _ = self.com_rc.Plan_Names(None, None, basedir)
-        return names
+        status, _, messages = self.com_rc.Compute_CurrentPlan(None, None)
+        return status, messages
 
+    # TODO - fix this to use plan objects
+    def set_plan(self, plan):
+        self.com_rc.Plan_SetCurrent(plan)
+
+    def show(self):
+        """
+        Makes RAS window visible
+        """
+        self.com_rc.ShowRas()
+
+    # Methods below here are semi-private and are intended to be called from the River, Reach, and Node classes
     def output_getnodes(self, river_id, reach_id):
         """
         Return node names (stationing) and node types
@@ -164,28 +195,37 @@ class RasController(object):
         _, _, reaches = self.com_rc.Output_GetReaches(river_num, None, None)
         return reaches
 
-    def output_nodeoutput(self, river_id, reach_id, node_id, value_type):
-        profile_id = 1 # TODO - fix this
+    def output_nodeoutput(self, river_id, reach_id, node_id, profile, value_type):
+        """
+        Return RAS node value
+        :param river_id: river code for node
+        :param reach_id: reach code for node
+        :param node_id: code for node
+        :param profile: code for desired profile
+        :param value_type: RAS value type (see constants at top of this file)
+        :return: float (probably)
+        """
         # TODO - the 0 in the next line should be a lot smarter
-        value = self.com_rc.Output_NodeOutput(river_id, reach_id, node_id, 0, profile_id, value_type)[0]
+        value = self.com_rc.Output_NodeOutput(river_id, reach_id, node_id, 0, profile, value_type)[0]
         return value
 
-    def run_current_plan(self):
+    # TODO - remove once obsolete - change this to work with Plan objects
+    def _current_plan_file(self):
         """
-        Run current plan in RAS
-        :return: status, messages - ??, ??
+        Returns path and file name of current plan file
+        :return: string
         """
-        status, _, messages = self.com_rc.Compute_CurrentPlan(None, None)
-        return status, messages
+        return self.com_rc.CurrentPlanFile()
 
-    def set_plan(self, plan):
-        self.com_rc.Plan_SetCurrent(plan)
+    def _plan_names(self, basedir=True):
+        """
+        returns list of plan names
+        :param basedir: ???? unknown
+        :return: list of strings
+        """
+        _, names, _ = self.com_rc.Plan_Names(None, None, basedir)
+        return names
 
-    def show(self):
-        """
-        Makes RAS window visible
-        """
-        self.com_rc.ShowRas()
 
     def _output_getprofiles(self):
         """
@@ -203,6 +243,7 @@ class RasController(object):
         _, rivers = self.com_rc.Output_GetRivers(0, None)
         return rivers
 
+    # TODO - remove this, only in for testing
     def _nodes(self, river_id, reach_id):
         """
         Gets list of reaches for river represented by self
@@ -215,6 +256,7 @@ class RasController(object):
 def main():
     rc = RasController()
     rc.open_project('x:/python/rascontroller/ras_model/HG.prj')
+    #rc.open_project('x:/python/rascontroller/ras_model/GHC.prj')
     print rc._current_plan_file()
     print rc._plan_names()
     #rc.show()
@@ -249,8 +291,9 @@ def main():
             print riv
             print reach
             for node in reach.nodes:
-                #print node, 'special=', node.special_code
-                print node, node.value(WSEL), node.value(MIN_CH_EL)
+                print node, node.value(profs[0], MIN_CH_EL)
+                for prof in profs:
+                    print prof.name, node.value(prof, WSEL)
 
 if __name__ == '__main__':
     main()
