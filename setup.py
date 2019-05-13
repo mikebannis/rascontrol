6001aac392ca84ebcd82bf6937ceb9475484f916
mikebannis <mikebannis@gmail.com>
2019-05-13 12:05:23 -0600

    version update

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='rascontrol', version='0.01', 
+setup(name='rascontrol', version='0.011', 
       description='Control RAS using win32 COM objects', 
       author='Mike Bannister', author_email='mikebannis@gmail.com', 
       packages=['rascontrol'])
